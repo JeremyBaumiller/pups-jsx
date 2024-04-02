5d688bf906476e9d5e80d39345b7e6266e4af450
JeremyBaumiller <jbaumil07@gmail.com>
2024-04-02 14:45:04 -0400

    initial commit

---
+++ Guided Practice/functions.js
@@ -0,0 +1,27 @@
+// 1. Unit test
+// expect there to be two numbers (x, y)
+// expect an error if any data type besides number is used.
+// expect the two numbers to return the correct product.
+// expect the return product to be a number.
+// 2.
+// expect array to have numbers as the data type.
+// expect code to determin if numbers are odd or evan.
+// expect odd numbers to join new array.
+// expect all numbers to be sorted into numerical order (highest to lowest).
+// unexpected inputs should return as error.
+// unexpected inputs could consist of, letters instead of numbers (returns NAN)
+// if multiples of the same odd number in the arrary, expect the number to return only once (ignore duplicates).
+
+//function test
+//1. If shopping cart is empty, expect an error message or prompt to begin scanning items.
+//2. Ask if user has account or would like to proceed as a 'guest'.
+//3. Give prompt to enter account information with phone number or email,
+// - If user does not have an account and would like to sign up prompt to enter information.
+//4. expect price/total to be shown when items are in cart.
+//5. expect a prompt to enter payment information.
+//6. expect an indicator to show how many items are in cart.
+//7. expect a possible discount code for first time buyers or special promotions.
+//8. expect tax to be applied to total.
+//9. expect a choice to increse or decrese amount of items in cart.
+//10. expect a shipping and handling charge and or prompt for shipping address.
+//11. expect a loyalty program or subscription service for items that are often purchased multiple times.
---
+++ Guided Practice/temp.js
@@ -0,0 +1,10 @@
+function convertCtof(celsius) {
+  let fahrenheit = celsius * (9 / 5) + 32;
+  console.log(fahrenheit);
+  return fahrenheit;
+}
+convertCtof(-30);
+convertCtof(-10);
+convertCtof(0);
+convertCtof(20);
+convertCtof(30);
